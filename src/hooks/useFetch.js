4bd2bb1d60ba2ce60e7ed8c9d0a279970089dcc0
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-11-29 18:09:32 +0530

    fdsfsd

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+function useFetch() {
+    useEffect(() => {
+        async function fetchPlaces() {
+          setIsFetching(true);
+          try {
+            const places = await fetchUserPlaces();
+            setUserPlaces(places);
+          } catch (error) {
+            setError({ message: error.message || 'Failed to fetch user places.' });
+          }
+    
+          setIsFetching(false);
+        }
+    
+        fetchPlaces();
+      }, []);
+}
